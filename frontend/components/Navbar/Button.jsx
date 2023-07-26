c887b6a85ef33502a02ace64bd9f0a8f3cf8c9ce
HieuMT249 <hieumt249@gmail.com>
2023-07-26 10:41:05 +0700

    HieuMT commit UI Home Page

---
+++ frontend/components/Navbar/Button.jsx
@@ -0,0 +1,14 @@
+function Button() {
+    return ( 
+       <div>
+            <button className="w-28 h-11 rounded-full bg-black text-white">
+                LOG IN
+            </button>
+            <button className="mx-5 w-28 h-11 rounded-full bg-white text-black">
+                SIGN UP
+            </button>
+       </div>
+     );
+}
+
+export default Button;
